c379fe31885f23d3443fdbdc2aab2aec6e69ad49
Rohan <rohansethi1999430@gmail.com>
2021-07-09 00:30:45 +0530

    Created Header Component and imported material core and material icons

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 
 import './App.css';
+import Header from './Header';
+
 
 function App() {
   return (
     <div className="app">
-      <h1>Tinder Clone</h1>
+      
+
+      <Header></Header>
     </div>
   );
 }
--- src/Header.js
+++ src/Header.js
@@ -1,9 +1,11 @@
 import React from 'react'
-
+import './Header.css'
+import PersonIcon from '@material-ui/icons/Person';
 function Header() {
     return (
-        <div>
-            
+        <div className="header">
+            <h1>I am Header</h1>
+            <PersonIcon></PersonIcon>
         </div>
     )
 }

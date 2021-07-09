d402aad947ac80f5f844ed7696d7d535ba44971b
Rohan <rohansethi1999430@gmail.com>
2021-07-09 22:58:21 +0530

    imported tinder logo and used flex box

--- src/Header.js
+++ src/Header.js
@@ -1,11 +1,19 @@
 import React from 'react'
 import './Header.css'
 import PersonIcon from '@material-ui/icons/Person';
+import IconButton from '@material-ui/core/IconButton';
 function Header() {
     return (
         <div className="header">
-            <h1>I am Header</h1>
-            <PersonIcon></PersonIcon>
+            
+            <IconButton>
+            <PersonIcon fontSize="large" className="header__icon"></PersonIcon>
+            </IconButton>
+            
+    <img className="header__logo"
+        src="https://1000logos.net/wp-content/uploads/2018/07/Tinder-logo.png" 
+        alt="" />
+
         </div>
     )
 }

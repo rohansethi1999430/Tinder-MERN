8cf2f9db0c3d357fd200ac47d60ec7a9778f3e3e
Rohan <rohansethi1999430@gmail.com>
2021-07-10 17:12:09 +0530

    Header Component Completed

--- src/Header.js
+++ src/Header.js
@@ -2,6 +2,7 @@ import React from 'react'
 import './Header.css'
 import PersonIcon from '@material-ui/icons/Person';
 import IconButton from '@material-ui/core/IconButton';
+import ForumIcon from '@material-ui/icons/Forum';
 function Header() {
     return (
         <div className="header">
@@ -13,6 +14,10 @@ function Header() {
     <img className="header__logo"
         src="https://1000logos.net/wp-content/uploads/2018/07/Tinder-logo.png" 
         alt="" />
+        <IconButton>
+            <ForumIcon fontSize="large" className="header__icon"></ForumIcon>
+        </IconButton>
+        
 
         </div>
     )

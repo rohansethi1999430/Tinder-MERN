7ddae44233c72dce944d9ca74448c71cbc6cb512
Rohan <rohansethi1999430@gmail.com>
2021-07-17 17:33:25 +0530

    Connction of backend data base to front end database using axios

--- src/axios.js
+++ src/axios.js
@@ -3,7 +3,7 @@ import axios from 'axios'
 
 const instance =axios.create(
     {
-        baseURL:'http://localhost:8001/'
+        baseURL:'https://tinder-backend-rohan.herokuapp.com/'
     }
 )
 export default instance
